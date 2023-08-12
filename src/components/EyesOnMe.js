36625d888a3a503b8fcba6d21b46b5ab04611e56
Meghan Carter <msc2010@yahoo.com>
2023-08-12 15:04:56 -0500

    lab completed

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,15 @@
-// Code EyesOnMe Component Here
+import React from "react";
+
+function EyesOnMe () {
+    function blurEvent () {
+        console.log("Hey! Eyes on me!")
+    }
+    function focusEvent () {
+        console.log("Good!")
+    }
+    return (
+        <button onBlur={blurEvent} onFocus={focusEvent}>Eyes on me</button>
+    )
+}
+
+export default EyesOnMe;
